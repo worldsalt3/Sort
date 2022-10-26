7785fa42d5cd5523b27abc1066eee0b7e5e647c6
Worldsalt3 <riliwanhazzan@gmail.com>
2022-10-26 16:40:41 +0100

    implement linked list sorting

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import { Sorter } from './Sorter'
 import { NumberCollection } from './NumbersCollection'
 import { CharactersCollection } from './CharactersCollection'
-
+import { LinkedList } from './LinkedList'
 
 const numbersCollection = new NumberCollection([10, 3, 5, -1, -2, 0])
 const charactersCollection = new CharactersCollection('Animal')
 const sorter = new Sorter(numbersCollection)  
 const sortCharacter = new Sorter(charactersCollection) 
+const linkedList = new LinkedList()
+const linkedSort = new Sorter(linkedList)
+linkedList.add(500)
+linkedList.add(10)
+linkedList.add(0)
+linkedList.add(-1)
+linkedList.add(0)
+
 
 sorter.sort()
 sortCharacter.sort()
+linkedSort.sort()
 console.log(numbersCollection.data)
 console.log(charactersCollection.data)
+linkedList.print()
